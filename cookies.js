638638a71ff76cf56e40dd6d7fde2e019830707d
Martin Rødvand <martin@rodvand.net>
2013-12-16 17:22:26 +0100

    First working version. Deletes all cookies at http://www.aftenposten.no

--- cookies.js
+++ cookies.js
@@ -1,19 +1,21 @@
-function totallyAwesome() {
-    console.log("Logging.");
-    alert("TESTE!");
-}
-
-function awesomeTask() {
-  totallyAwesome();
+function deleteCookies() {
+    var domain = "http://www.aftenposten.no/";
+    chrome.cookies.getAll({"url" : domain}, function(cookies) {
+     console.log("Number of cookies: "+cookies.length);
+     for(var i = 0; i < cookies.length; i++) {
+        console.log("URL: "+ domain + " Name: " + cookies[i].name);
+        chrome.cookies.remove({"url" : domain, "name" : cookies[i].name});
+     }   
+    });
 }
 
 function clickHandler(e) {
-    awesomeTask();
+    deleteCookies();
 }
 
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', clickHandler);
+  document.querySelector('button').addEventListener('click', deleteCookies);
 });
